ec285ddf23f724082c546083809b9fdd2ba9198f
IvanReollo <ireollo12@gmail.com>
2024-10-25 20:35:01 +0200

    appUImagen

---
+++ Js/app.js
@@ -0,0 +1,13 @@
+for (const key in tareas) {
+    let li = document.createElement('li');
+    li.textContent = tareas[key].titulo;
+    if(tareas[key].completada){
+        li.setAttribute('class','t');
+    } else {
+        li.setAttribute('class','f');
+    }
+    ListaOrdenada.appendChild(li);   
+}
+let img = document.createElement('img');
+img.setAttribute('src','/img/1.png');
+frame.appendChild(img);

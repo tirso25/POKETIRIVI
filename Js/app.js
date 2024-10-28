e6378b9c9d7c38ec6ded1cb8544ca137d3747572
IvanReollo <ireollo12@gmail.com>
2024-10-28 18:23:49 +0100

    (mal)

--- Js/app.js
+++ Js/app.js
@@ -3,3 +3,7 @@
 let img = document.createElement('img');
 img.src = "./img/1.png";
 frame.appendChild(img)
+for (const key in pokemon) {
+    let p = document.createElement('p');
+    p.textContent()
+}
